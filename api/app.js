09717b50eca8b9fc09afc9572edad9996db24349
Trevor Blades <tdblades@gmail.com>
2020-04-29 19:51:06 -0700

    Reorganize API code

--- api/app.js
+++ api/app.js
@@ -2,24 +2,24 @@ const io = require('socket.io')(process.env.PORT);
 const redis = require('redis');
 const client = redis.createClient();
 
+const LIST_NAME = 'test';
+
+function displayList() {
+  client.lrange([LIST_NAME, 0, -1], (err, res) => {
+    io.emit('data', {customers: res});
+  });
+}
+
 io.on('connection', socket => {
-  client.lrange(['test', 0, -1], function(err, res) {
+  client.lrange([LIST_NAME, 0, -1], (err, res) => {
     socket.emit('data', {customers: res});
   });
 
   socket.on('name', data => {
-    client.rpush(['test', data.name], function() {
-      client.lrange(['test', 0, -1], function(err, res) {
-        io.emit('data', {customers: res});
-      });
-    });
+    client.rpush([LIST_NAME, data.name], displayList);
   });
 
   socket.on('next', () => {
-    client.lpop('test', function() {
-      client.lrange(['test', 0, -1], function(err, res) {
-        io.emit('data', {customers: res});
-      });
-    });
+    client.lpop(LIST_NAME, displayList);
   });
 });
