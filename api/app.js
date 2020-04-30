8b793d2423c1c349603cc2787beb20432138dcc4
Trevor Blades <tdblades@gmail.com>
2020-04-29 19:22:17 -0700

    replaced variable state with redis

--- api/app.js
+++ api/app.js
@@ -1,12 +1,17 @@
 const io = require('socket.io')(process.env.PORT);
-
-const customers = ['Customer B', 'Customer C', 'Customer D'];
+const redis = require('redis');
+const client = redis.createClient();
 
 io.on('connection', socket => {
-  socket.emit('data', {customers});
+  client.lrange(['test', 0, -1], function(err, res) {
+    socket.emit('data', {customers: res});
+  });
 
   socket.on('name', data => {
-    customers.push(data.name);
-    io.emit('data', {customers});
+    client.rpush(['test', data.name], function() {
+      client.lrange(['test', 0, -1], function(err, res) {
+        io.emit('data', {customers: res});
+      });
+    });
   });
 });
