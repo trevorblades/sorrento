3bdc64775c2230373750a33b2324edf480274511
Trevor Blades <tdblades@gmail.com>
2020-06-12 12:13:27 -0700

    Move message form into its own component

--- client/src/components/OrganizationSettings.js
+++ client/src/components/OrganizationSettings.js
@@ -1,4 +1,5 @@
 import ChatBubble from './ChatBubble';
+import MessageForm from './MessageForm';
 import PropTypes from 'prop-types';
 import React, {useState} from 'react';
 import {
@@ -16,88 +17,10 @@ import {
   NumberInputField,
   NumberInputStepper,
   Select,
-  Stack,
-  Textarea
+  Stack
 } from '@chakra-ui/core';
 import {createWelcomeMessage} from 'common';
 import {format} from 'phone-fns';
-
-function MessageForm(props) {
-  switch (props.message) {
-    case 'welcomeMessage':
-      return (
-        <Stack spacing="4">
-          <FormControl>
-            <Textarea
-              resize="none"
-              name="welcomeMessage"
-              onChange={props.onInputChange}
-              value={props.organization.welcomeMessage}
-            />
-            <FormHelperText>
-              You may use the &#123;KEYWORD&#125; and &#123;QUEUE_MESSAGE&#125;
-              variables in this message
-            </FormHelperText>
-          </FormControl>
-          <FormControl>
-            <FormLabel>
-              &#123;QUEUE_MESSAGE&#125; variable when there are people in the
-              queue
-            </FormLabel>
-            <Textarea
-              resize="none"
-              name="queueMessage"
-              onChange={props.onInputChange}
-              value={props.organization.queueMessage}
-            />
-            <FormHelperText>
-              You may use the &#123;IS&#125;, &#123;PERSON&#125;, and
-              &#123;ESTIMATED_WAIT_TIME&#125; variables in this message
-            </FormHelperText>
-          </FormControl>
-          <FormControl>
-            <FormLabel>
-              &#123;QUEUE_MESSAGE&#125; variable when the queue is empty
-            </FormLabel>
-            <Input
-              name="queueEmptyMessage"
-              onChange={props.onInputChange}
-              value={props.organization.queueEmptyMessage}
-            />
-          </FormControl>
-        </Stack>
-      );
-    case 'notAcceptingMessage':
-      return (
-        <>
-          <Textarea
-            resize="none"
-            name="notAcceptingMessage"
-            onChange={props.onInputChange}
-            value={props.organization.notAcceptingMessage}
-          />
-          <FormHelperText>
-            The message sent to customers when your waitlist is not accepting
-            new customers
-          </FormHelperText>
-        </>
-      );
-    default:
-      return (
-        <Input
-          onChange={props.onInputChange}
-          name={props.message}
-          value={props.organization[props.message]}
-        />
-      );
-  }
-}
-
-MessageForm.propTypes = {
-  message: PropTypes.string.isRequired,
-  organization: PropTypes.object.isRequired,
-  onInputChange: PropTypes.func.isRequired
-};
 
 const defaults = {
   keyword: 'REMOVE'
@@ -211,11 +134,16 @@ export default function OrganizationSettings(props) {
           </Box>
           <Box>
             <Heading fontSize="3xl" mb="6">
-              Automated responses
+              Configure responses
             </Heading>
             <Flex align="flex-start">
               <Box flexShrink="0" mr="5" w="full" maxW="containers.sm">
-                <Select mb="4" onChange={handleMessageChange} value={message}>
+                <Select
+                  size="lg"
+                  mb="4"
+                  onChange={handleMessageChange}
+                  value={message}
+                >
                   <option value="welcomeMessage">Welcome</option>
                   <option value="notAcceptingMessage">Not accepting</option>
                   <option value="removedMessage">Removed</option>
