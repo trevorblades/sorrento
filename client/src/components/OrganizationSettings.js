3580b0508c0df4c6bb71279293f523bcd97426da
Trevor Blades <tdblades@gmail.com>
2020-06-12 00:12:44 -0700

    Add some sections and spacing

--- client/src/components/OrganizationSettings.js
+++ client/src/components/OrganizationSettings.js
@@ -26,7 +26,7 @@ function MessageForm(props) {
   switch (props.message) {
     case 'welcomeMessage':
       return (
-        <>
+        <Stack spacing="4">
           <FormControl>
             <Textarea
               resize="none"
@@ -65,7 +65,7 @@ function MessageForm(props) {
               value={props.organization.queueEmptyMessage}
             />
           </FormControl>
-        </>
+        </Stack>
       );
     case 'notAcceptingMessage':
       return (
@@ -99,15 +99,13 @@ MessageForm.propTypes = {
   onInputChange: PropTypes.func.isRequired
 };
 
-const DEFAULT_KEYWORD = 'REMOVE';
+const defaults = {
+  keyword: 'REMOVE'
+};
 
 export default function OrganizationSettings(props) {
   const [message, setMessage] = useState('welcomeMessage');
   const [organization, setOrganization] = useState(props.data.organization);
-
-  function handleSubmit(event) {
-    event.preventDefault();
-  }
 
   function handleMessageChange(event) {
     setMessage(event.target.value);
@@ -124,126 +122,133 @@ export default function OrganizationSettings(props) {
   return (
     <>
       <Box p={[5, 6]}>
-        <Box mx="auto" maxW="containers.lg" as="form" onSubmit={handleSubmit}>
-          <Heading mb="6" fontSize="3xl">
-            Organization settings
-          </Heading>
-          <Stack maxW="containers.sm" spacing="4">
-            <FormControl>
-              <FormLabel>Organization name</FormLabel>
-              <Input defaultValue={organization.name} />
-            </FormControl>
-            <FormControl>
-              <FormLabel>SMS number</FormLabel>
-              <Input
-                value={format('(NNN) NNN-NNNN', organization.phone.slice(2))}
-                isDisabled
-              />
-              <FormHelperText>This can&apos;t be changed</FormHelperText>
-            </FormControl>
-            {/* TODO: use sliders for numbers */}
-            <FormControl>
-              <FormLabel>Queue limit</FormLabel>
-              <NumberInput
-                min={1}
-                max={100}
-                isRequired
-                name="queueLimit"
-                defaultValue={organization.queueLimit}
+        <Stack spacing="12" mx="auto" maxW="containers.lg">
+          <Box>
+            <Heading mb="6" fontSize="3xl">
+              Organization settings
+            </Heading>
+            <Stack maxW="containers.sm" spacing="4">
+              <FormControl>
+                <FormLabel>Organization name</FormLabel>
+                <Input defaultValue={organization.name} />
+              </FormControl>
+              <FormControl>
+                <FormLabel>SMS number</FormLabel>
+                <Input
+                  value={format('(NNN) NNN-NNNN', organization.phone.slice(2))}
+                  isDisabled
+                />
+                <FormHelperText>This can&apos;t be changed</FormHelperText>
+              </FormControl>
+              {/* TODO: use sliders for numbers */}
+              <FormControl>
+                <FormLabel>Queue limit</FormLabel>
+                <NumberInput
+                  min={1}
+                  max={100}
+                  isRequired
+                  name="queueLimit"
+                  defaultValue={organization.queueLimit}
+                >
+                  <NumberInputField />
+                  <NumberInputStepper>
+                    <NumberIncrementStepper />
+                    <NumberDecrementStepper />
+                  </NumberInputStepper>
+                </NumberInput>
+              </FormControl>
+              <FormControl>
+                <FormLabel>Active agents</FormLabel>
+                <NumberInput
+                  min={1}
+                  name="activeAgents"
+                  isRequired
+                  defaultValue={organization.activeAgents}
+                >
+                  <NumberInputField />
+                  <NumberInputStepper>
+                    <NumberIncrementStepper />
+                    <NumberDecrementStepper />
+                  </NumberInputStepper>
+                </NumberInput>
+                <FormHelperText>
+                  The number of people serving customers
+                </FormHelperText>
+              </FormControl>
+              <FormControl>
+                <FormLabel>Average handle time</FormLabel>
+                <NumberInput
+                  min={1}
+                  name="averageHandleTime"
+                  isRequired
+                  defaultValue={organization.averageHandleTime}
+                >
+                  <NumberInputField />
+                  <NumberInputStepper>
+                    <NumberIncrementStepper />
+                    <NumberDecrementStepper />
+                  </NumberInputStepper>
+                </NumberInput>
+                <FormHelperText>
+                  The average amount of time (in minutes) to finish serving a
+                  customer
+                </FormHelperText>
+              </FormControl>
+              <FormControl>
+                <FormLabel>Self-remove keyword</FormLabel>
+                <Input
+                  name="keyword"
+                  placeholder={defaults.keyword}
+                  onChange={handleInputChange}
+                  value={organization.keyword}
+                />
+                <FormHelperText>
+                  The keyword that your customers can text to remove themselves
+                  from your waitlist (case-insensitive)
+                </FormHelperText>
+              </FormControl>
+            </Stack>
+          </Box>
+          <Box>
+            <Heading fontSize="3xl" mb="6">
+              Automated responses
+            </Heading>
+            <Flex align="flex-start">
+              <Box flexShrink="0" mr="5" w="full" maxW="containers.sm">
+                <Select mb="4" onChange={handleMessageChange} value={message}>
+                  <option value="welcomeMessage">Welcome</option>
+                  <option value="notAcceptingMessage">Not accepting</option>
+                  <option value="removedMessage">Removed</option>
+                  <option value="notRemovedMessage">Not removed</option>
+                  <option value="limitExceededMessage">Limit exceeded</option>
+                </Select>
+                <MessageForm
+                  message={message}
+                  organization={organization}
+                  onInputChange={handleInputChange}
+                />
+              </Box>
+              <Flex
+                direction="column"
+                px="7px"
+                overflow="hidden"
+                mx="auto"
+                w="full"
               >
-                <NumberInputField />
-                <NumberInputStepper>
-                  <NumberIncrementStepper />
-                  <NumberDecrementStepper />
-                </NumberInputStepper>
-              </NumberInput>
-            </FormControl>
-            <FormControl>
-              <FormLabel>Active agents</FormLabel>
-              <NumberInput
-                min={1}
-                name="activeAgents"
-                isRequired
-                defaultValue={organization.activeAgents}
-              >
-                <NumberInputField />
-                <NumberInputStepper>
-                  <NumberIncrementStepper />
-                  <NumberDecrementStepper />
-                </NumberInputStepper>
-              </NumberInput>
-              <FormHelperText>
-                The number of people serving customers
-              </FormHelperText>
-            </FormControl>
-            <FormControl>
-              <FormLabel>Average handle time</FormLabel>
-              <NumberInput
-                min={1}
-                name="averageHandleTime"
-                isRequired
-                defaultValue={organization.averageHandleTime}
-              >
-                <NumberInputField />
-                <NumberInputStepper>
-                  <NumberIncrementStepper />
-                  <NumberDecrementStepper />
-                </NumberInputStepper>
-              </NumberInput>
-              <FormHelperText>
-                The average amount of time (in minutes) to finish serving a
-                customer
-              </FormHelperText>
-            </FormControl>
-            <FormControl>
-              <FormLabel>Self-remove keyword</FormLabel>
-              <Input
-                name="keyword"
-                placeholder={DEFAULT_KEYWORD}
-                onChange={handleInputChange}
-                value={organization.keyword}
-              />
-              <FormHelperText>
-                The keyword that your customers can text to remove themselves
-                from your waitlist (case-insensitive)
-              </FormHelperText>
-            </FormControl>
-          </Stack>
-          <Flex align="flex-start">
-            <Box flexShrink="0" mr="4" w="full" maxW="containers.sm">
-              <Select onChange={handleMessageChange} value={message}>
-                <option value="welcomeMessage">Welcome</option>
-                <option value="notAcceptingMessage">Not accepting</option>
-                <option value="removedMessage">Removed</option>
-                <option value="notRemovedMessage">Not removed</option>
-                <option value="limitExceededMessage">Limit exceeded</option>
-              </Select>
-              <MessageForm
-                message={message}
-                organization={organization}
-                onInputChange={handleInputChange}
-              />
-            </Box>
-            <Flex
-              direction="column"
-              px="7px"
-              overflow="hidden"
-              mx="auto"
-              w="full"
-            >
-              <ChatBubble>
-                {/[rR]emovedMessage$/.test(message)
-                  ? organization.keyword || DEFAULT_KEYWORD
-                  : 'Bonnie'}
-              </ChatBubble>
-              <ChatBubble fromThem>
-                {message === 'welcomeMessage'
-                  ? createWelcomeMessage(organization, 1)
-                  : organization[message]}
-              </ChatBubble>
+                <ChatBubble>
+                  {/[rR]emovedMessage$/.test(message)
+                    ? organization.keyword || defaults.keyword
+                    : 'Bonnie'}
+                </ChatBubble>
+                <ChatBubble fromThem>
+                  {message === 'welcomeMessage'
+                    ? createWelcomeMessage(organization, 1)
+                    : organization[message]}
+                </ChatBubble>
+              </Flex>
             </Flex>
-          </Flex>
-        </Box>
+          </Box>
+        </Stack>
       </Box>
       <Box
         px="4"
@@ -257,7 +262,7 @@ export default function OrganizationSettings(props) {
         bg="white"
       >
         <Box mx="auto" maxW="containers.lg">
-          <Button type="submit" size="lg" variantColor="green">
+          <Button size="lg" variantColor="green">
             Save changes
           </Button>
         </Box>
