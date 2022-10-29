8203c08e430319b84b686aa91b74e1d8d7a10dfb
Trevor Blades <tdblades@gmail.com>
2022-10-29 14:49:31 -0700

    List and subscribe to customers

--- pages/list.tsx
+++ pages/list.tsx
@@ -1,18 +1,50 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Button } from "@chakra-ui/react";
+import { Database } from "../database.types";
 import { InferGetServerSidePropsType, NextPage } from "next";
+import { RealtimeChannel } from "@supabase/supabase-js";
 import { useRouter } from "next/router";
 import { useSupabaseClient } from "@supabase/auth-helpers-react";
 import { withPageAuth } from "@supabase/auth-helpers-nextjs";
 
-export const getServerSideProps = withPageAuth({ redirectTo: "/login" });
+export const getServerSideProps = withPageAuth<Database>({
+  redirectTo: "/login",
+  async getServerSideProps(_, supabase) {
+    const { data } = await supabase
+      .from("customers")
+      .select("*")
+      .order("id", { ascending: false });
+    return {
+      props: {
+        customers: data,
+      },
+    };
+  },
+});
 
 const List: NextPage<
   InferGetServerSidePropsType<typeof getServerSideProps>
-> = ({ user }) => {
+> = ({ user, customers: defaultCustomers }) => {
+  const [loading, setLoading] = useState(false);
+
   const router = useRouter();
   const supabase = useSupabaseClient();
-  const [loading, setLoading] = useState(false);
+
+  const [customers, setCustomers] = useState(defaultCustomers);
+
+  useEffect(() => {
+    const channel = supabase
+      .channel("table-db-changes")
+      .on(
+        "postgres_changes",
+        { event: "INSERT", schema: "public", table: "customers" },
+        (payload) => {
+          setCustomers((prev: any) => [payload.new, ...prev]);
+        }
+      )
+      .subscribe();
+    console.log(channel);
+  }, []);
 
   return (
     <div>
@@ -31,6 +63,11 @@ const List: NextPage<
       >
         log out
       </Button>
+      <ul>
+        {customers.map((customer) => (
+          <li key={customer.id}>{customer.name}</li>
+        ))}
+      </ul>
     </div>
   );
 };
