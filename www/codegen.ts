f66f98f44e06b25a6fdb3392dd606a0950a33862
Trevor Blades <tdblades@gmail.com>
2022-11-23 00:00:40 -0800

    Use the schema again

--- www/codegen.ts
+++ www/codegen.ts
@@ -2,10 +2,7 @@ import { CodegenConfig } from "@graphql-codegen/cli";
 
 const config: CodegenConfig = {
   overwrite: true,
-  schema:
-    process.env.NODE_ENV === "production"
-      ? "https://api.sorrentobarbers.com/graphql"
-      : "http://localhost:4000/graphql",
+  schema: "../api/schema.graphql",
   documents: "src/**/*.graphql",
   generates: {
     "src/generated/graphql.tsx": {
